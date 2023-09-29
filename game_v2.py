984b1e951df73392f4d11928f03aedbd65ede5bc
Mikhail Chenchak <vtb4055920@region.vtb.ru>
2023-09-29 16:24:38 +0300

    Функция 'рандомного' угадывания заменена рекурсивной функцией бинарного поиска.

--- game_v2.py
+++ game_v2.py
@@ -5,40 +5,45 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+def recursive_predict(low: int, hight: int, number: int = 1) -> int:
+    """Рекурсивный поиск числа
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
+        low (int, mandatory): Нижняя граница поиска.
+        hight (int, mandatory): Верхняя граница поиска.
 
     Returns:
         int: Число попыток
     """
-    count = 0
 
-    while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict_number:
-            break  # выход из цикла если угадали
-    return count
+    mid = (low + hight) // 2
+
+    if mid == number:
+        return 1
+
+    if mid > number:
+        return 1 + recursive_predict(low, mid - 1, number)
+
+    elif mid < number:
+        return 1 + recursive_predict(mid + 1, hight, number)
 
 
-def score_game(random_predict) -> int:
+def score_game(predict_func) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        recursive_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(recursive_predict(0, 100, number))
 
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
@@ -47,4 +52,4 @@ def score_game(random_predict) -> int:
 
 if __name__ == "__main__":
     # RUN
-    score_game(random_predict)
+    score_game(recursive_predict)
